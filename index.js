6cfa8dc3cdc23a0aa83f617c3971681d1271a566
Admir Alicic <admir_alicic@yahoo.com>
2016-07-21 14:59:27 +0200

    search done

--- index.js
+++ index.js
@@ -2,14 +2,38 @@ var users = [
     {ID: 1, "Full Name" : "Admir Alicic", Username: "admiralicic", "E-mail": "admir.alicic@gmail.com", Password: "test"},
     {ID: 2, "Full Name" : "Emir Alicic", Username: "emiralicic", "E-mail": "emir.alicic@gmail.com", Password: "test"},
     {ID: 3, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 4, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 5, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 6, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 7, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 8, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 9, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 10, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 11, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 12, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
+    {ID: 13, "Full Name" : "Haris Alicic", Username: "harisalicic", "E-mail": "haris.alicic@gmail.com", Password: "test"},
 ];
 
 function navigate(state){
+    hideError();
     var pages = document.getElementsByClassName("page");
     for(var i = 0; i < pages.length; i++){
         pages[i].id === state ? pages[i].className = "page container" : pages[i].className = " page container hiddenPage";
     }
    
+}
+
+function search(event){
+    if(event.keyCode === 13){
+        var criteria = event.target.value;
+        var filteredUsers = users.filter(function(user){
+            if(user["Full Name"].indexOf(criteria) > -1 || user.Username.indexOf(criteria) > -1){
+                return user;
+            }
+        });
+
+        rebuildTable(filteredUsers);
+    }
 }
 
 function editUser(event){
@@ -40,7 +64,17 @@ function updateUser(){
            users[i]["E-mail"] = document.getElementById("editEmail").value;
            users[i]["Password"] = document.getElementById("editPassword").value;
 
-           rebuildTable();
+           if(!validUser(users[i])){
+               displayError("All fields are required!");
+               return;
+           }
+
+           if(userExists(users[i])){
+               displayError("Username or E-mail already taken!");
+               return;
+           }
+
+           rebuildTable(users);
            break;
        }
     }
@@ -77,29 +111,64 @@ function rowData(row){
     }
 }
 
+function userExists(user){
+    for(var i = 0; i < users.length; i++){
+        if(users[i].Username == user.Username || users[i]["E-mail"] == user["E-mail"]){
+            if(users[i].ID != user.ID){
+                return true;
+            }
+        }
+    }
+}
+
+function validUser(user){
+
+    var isValid = true;
+
+    if(user["Full Name"] == "" || user["Username"] == "" || user["E-mail"] == "" || user["Password"] == ""){
+        isValid = false; 
+    } 
+
+    return isValid;
+}
+
+function displayError(message){
+    var alertBox = document.getElementById("userAlert");
+    alertBox.className = "alert alert-danger";
+    alertBox.textContent = message;
+}
+
+function hideError(){
+    var alertBox = document.getElementById("userAlert");
+    alertBox.className = "alert alert-danger hide";
+}
 function create() {
     var newUser = {};
     newUser["Full Name"] = document.getElementById('fullName').value;
     newUser["Username"] = document.getElementById('username').value;
     newUser["E-mail"] = document.getElementById('email').value;
     newUser["Password"] = document.getElementById('password').value;
+    
+    var alertBox = document.getElementById("createUserAlert");
+
+    if(!validUser(newUser)){
+        displayError("All fields are required!");
+        return;
+    }
+
+    if(userExists(newUser)){
+        displayError("Username or E-mail already taken!");
+        return;
+    }
 
     newUser["ID"] = users[users.length-1].ID + 1;
     users.push(newUser);
 
-    // var table = document.getElementById("usersTable");
-
-    // if(table){
-    //     var tablePage = document.getElementById("listContainer");
-    //     tablePage.removeChild(table);
-    // }
-
-    rebuildTable();
-    // list(users);
+    rebuildTable(users);
     navigate('list');
 }
 
-function rebuildTable(){
+function rebuildTable(usersList){
     var table = document.getElementById("usersTable");
 
     if(table){
@@ -107,7 +176,7 @@ function rebuildTable(){
         tablePage.removeChild(table);
     }
 
-    list(users);
+    list(usersList);
 }
 
 function list(users) {
